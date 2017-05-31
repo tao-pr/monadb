27001046c3ee02a88a72e3d8b9ed1ac1cd5a84be
Tao PR <pataoengineer@gmail.com>
2017-05-31 22:43:59 +0700

    db : use warn in abstract methods over log

--- interface/db.js
+++ interface/db.js
@@ -18,37 +18,37 @@ class DBInterface {
   }
 
   find(cond){
-    console.log('[find] is not implemented.'.yellow);
+    console.warn('[find] is not implemented.'.yellow);
     return Promise.resolve(this);
   }
 
   insert(rec){
-    console.log('[insert] is not implemented.'.yellow);
+    console.warn('[insert] is not implemented.'.yellow);
     return Promise.resolve(this);
   }
 
   update(cond, updater){
-    console.log('[update] is not implemented.'.yellow);
+    console.warn('[update] is not implemented.'.yellow);
     return Promise.resolve(this);
   }
 
   count(cond){
-    console.log('[count] is not implemented'.yellow);
+    console.warn('[count] is not implemented'.yellow);
     return Promise.resolve(this);
   }
 
   delete(cond){
-    console.log('[delete] is not implemented.'.yellow);
+    console.warn('[delete] is not implemented.'.yellow);
     return Promise.resolve(this);
   }
 
   drop(){
-    console.log('[drop] is not implemented.'.yellow);
+    console.warn('[drop] is not implemented.'.yellow);
     return Promise.resolve(this);
   }
 
   forEach(f){
-    console.log('[forEach] is not implemented.'.yellow);
+    console.warn('[forEach] is not implemented.'.yellow);
     return Promise.resolve(this);
   }
 }
